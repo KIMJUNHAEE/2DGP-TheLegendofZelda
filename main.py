24056e3e478cd5510aed0f15464425bce44dc812
KIMJUNHAEE <kkjjhh528@naver.com>
2025-11-10 12:33:50 +0900

    상단 스크린을 위해 화면 크기 조정

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import game_framework
 
 import play_mode as start_mode
 
+# 게임 화면 크기 설정
+# 플레이 화면 1280 x 880
+# 상단 화면 1280 x 140
 game_weight = 1280
-game_height = 880
+game_height = 1020
 
 open_canvas(game_weight, game_height)
 game_framework.run(start_mode)

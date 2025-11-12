fb3ecdec13da08e61f1b9716676f4cdb026b94b3
KIMJUNHAEE <kkjjhh528@naver.com>
2025-11-12 22:08:42 +0900

    칼 크기 변경

--- Item.py
+++ Item.py
@@ -24,20 +24,20 @@ class Item:
 
     def draw(self):
         if self.item_type == 'sword':
-            self.image.clip_draw(0, 0, 7, 16, self.x, self.y, 32, 32)
+            self.image.clip_draw(0, 0, 7, 16, self.x, self.y, 24, 50)
 
         if config.Show_BB:
             draw_rectangle(*self.get_bb())
 
     def get_bb(self):
         if self.item_type == 'sword':
-            return self.x - 16, self.y - 16, self.x + 16, self.y + 16
+            return self.x - 12, self.y - 25, self.x + 12, self.y + 25
 
     def handle_collision(self, group, other):
         if group == 'player:item' and not self.should_remove:
             self.remove_time = get_time() + 2.0  # 2초 후 제거
             self.should_remove = True
             self.x = self.player.x - 20
-            self.y = self.player.y + 40
+            self.y = self.player.y + 45
 
 
